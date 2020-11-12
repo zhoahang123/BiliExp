89c47f0629814fd1a4ac8003942f6d119a00bd2d
Tim Mingxing <tim.tt24@gmail.com>
2020-11-12 19:52:22 +0800

    优化配置文件生成

--- secrets2config.py
+++ secrets2config.py
@@ -25,26 +25,32 @@ if BILICONFIG:
     users = []
     cookieDatas = {}
     for x in BILICONFIG.split("\n"):
-        if re.match("[a-z 0-9]{8}%2C[0-9]{10}%2C[a-z 0-9]{5}\*[a-z 0-9]{2}", x):
-            cookieDatas["SESSDATA"] = x
+        cookie = x.strip().replace(",", "%2C")
+        if re.match("[a-z0-9]{8}%2C[0-9a-z]{10}%2C[a-z0-9]{5}\*[a-z0-9]{2}", cookie):
+            cookieDatas["SESSDATA"] = cookie
             SESSDATA = True
-        elif re.match("[a-z 0-9]{31}", x):
-            cookieDatas["bili_jct"] = x
+        elif re.match("[a-z 0-9]{31}", cookie):
+            cookieDatas["bili_jct"] = cookie
             bili_jct = True
-        elif re.match("^[0-9]*$", x):
-            cookieDatas["DedeUserID"] = x
+        elif re.match("^[0-9]*$", cookie):
+            cookieDatas["DedeUserID"] = cookie
             DedeUserID = True
         if SESSDATA and bili_jct and DedeUserID:
             users.append({"cookieDatas": cookieDatas.copy(), "tasks": {}})
             SESSDATA, bili_jct, DedeUserID = False, False, False
-    configData["users"] = users
+    if len(users) == 0:
+        print("虽然配置了BILICONFIG，但并没有发现有效账户cookie，如有必要请使用首页的复杂部署模式，并配置ADVCONFIG")
+        exit(-1)
+    else:
+        configData["users"] = users
 
 if PUSH_MESSAGE:
     for x in PUSH_MESSAGE.split("\n"):
+        value = x.strip()
         if x.startswith("SCU"):
-            configData["SCKEY"] = x
-        elif re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", x):
-            configData["email"] = x
+            configData["SCKEY"] = value
+        elif re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", value):
+            configData["email"] = value
 
 with open('./config/config.json','w',encoding='utf-8') as fp:
     json.dump(configData, fp, ensure_ascii=False)
